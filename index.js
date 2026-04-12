e2b1560fb3d639efae16a94a0e418f36540beac3
Lubna <lubnasuleman94@gmail.com>
2026-04-12 12:32:55 +0500

    Commit no-2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Lubna")
+const accountId = 200414706
+let accountEmail = "anbul@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Lahore" 
+
+// accountId =2 // not allowed 
+
+accountEmail = "lubna@gmail.com"
+accountPassword = "67890"
+accountCity = "Karachi"
+
+console.log("accountId = Bc200414706");
+
+console.table(["accountEmail= lubna@gmail.com", accountPassword = "67890", accountCity = "Karachi"]);
