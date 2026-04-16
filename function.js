382b9246c59e8c122b91e0bf6b20c225e131bcd0
Lubna <lubnasuleman94@gmail.com>
2026-04-16 13:48:21 +0500

    functions--3

---
+++ function.js
@@ -0,0 +1,34 @@
+
+function sayMyName(){
+console.log("L");
+console.log("U");
+console.log("B");
+console.log("N");
+console.log("A");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+// console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+ 
+   // let result number1 + number2
+   // return result 
+}
+const result = addTwoNumbers(3, 7)
+
+// console.log("Result: ", result);
+
+function loginuserMessage(username = "anbul"){
+    if (username === undefined) {
+        console.log("Please enter a username");
+        return
+    }
+    return  '${Lubna} just logged in'
+}
+
+// console.log(loginuserMessage("Lubna"));
+ console.log(loginuserMessage("Lubna"));
