934cc534984c50545607f25e6e854cb266ae98b6
Lubna <lubnasuleman94@gmail.com>
2026-04-23 12:06:37 +0500

    commit=no3 one.js

---
+++ one.js
@@ -0,0 +1,33 @@
+// const temperature = 41 
+// if (temperature === 40) {
+//     console.log("less then 50");
+//  } else{
+//         console.log("temperature is greater then 50");
+//     }
+    
+
+// console.log("Execute");
+
+
+// < , > , <=,>=, ==, =, !=, !==
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log("user power: fly");
+    
+// }
+
+// const userLoggedIn = true
+// const userdebitcard = true
+// const userLoggedInFromGoogle = false
+// const userLoggedInFromEmail = true
+
+// if (userLoggedIn && userdebitcard && 2==2) {
+//     console.log("Allow to buy course");    
+// }
+// if (userLoggedInFromGoogle || userLoggedInFromEmail) {
+//     console.log("User Logged In");
+    
+    
+// }
